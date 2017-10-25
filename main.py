bbe28490ab053f8de96ebc0fa104905b10b956b0
shawnwang-tech <shawnwang.tech@gmail.com>
2017-10-25 21:44:58 +0800

    multiprecess

--- main.py
+++ main.py
@@ -7,23 +7,34 @@ Read wiki data, generate word distance matrix and word counts matrix.
 
 import re
 import os
+import glob
+import time
+import datetime
 import numpy as np
 from stop_words import get_stop_words
 from multiprocessing import Pool
 
 
 WINDOW_SIZE = 5
-CORPUS_DIR = '../data/enwiki_test/'
+
+dir_prefix = '/home/shawnwang/workspace/research/data/enwiki/%s/'
+tag = ['EH', 'DT', 'DK', 'CZ', 'AW', 'DW', 'BG', 'BR', 'BD', 'CW', 'BU', 'EG', 'AM', 'DG', 'DS', 'BE', 'CR', 'ED', 'BI', 'DF', 'DE', 'EO', 'BK', 'EK', 'DD', 'AJ', 'DP', 'DZ', 'DI', 'AY', 'CU', 'AR', 'EA', 'EE', 'AV', 'CS', 'BF', 'DR', 'BN', 'BO', 'DB', 'AK', 'DX', 'BJ', 'AZ', 'BS', 'EM', 'DN', 'CQ', 'DY', 'EN', 'AS', 'AP', 'DA', 'DU', 'CP', 'CV', 'BC', 'BM', 'EB', 'DQ', 'DV', 'EC', 'DO', 'BH', 'DM', 'DL', 'BA', 'AL', 'BB', 'CX', 'CY', 'DC', 'AN', 'AU', 'BP', 'AX', 'AQ', 'AT', 'DJ', 'EF', 'EL', 'CT', 'EP', 'AO', 'BL', 'AH', 'BQ', 'EJ', 'BT', 'EI', 'DH', 'AI']
+
+
 VOCABULARY_FILE = './10k.txt'
+PKL_PATH = './pkl_dir'
 
 
 class CalDist(object):
 
-    def __init__(self, dimlen, window):
+    def __init__(self, dimlen, window, pkl_path=None):
         self.dists = np.zeros((dimlen, dimlen), dtype=np.float64)
         self.counts = np.zeros((dimlen, dimlen), dtype=np.float64)
-        self.dists_avg = np.zeros((dimlen, dimlen), dtype=np.float64)
+        #self.dists_avg = np.zeros((dimlen, dimlen), dtype=np.float64)
         self.window = window
+        if pkl_path is not None:
+            #self.dists, self.counts, self.counts = self.load_matrix(pkl_path)
+            self.dists, self.counts = self.load_matrix(pkl_path)
        
     def traverse(self, line_words, vocab):
         line_len = len(line_words)
@@ -56,6 +67,18 @@ class CalDist(object):
         self.dists_avg = np.true_divide(self.dists, self.counts)
         dist_sum = np.sum(self.dists_avg)
         self.dists_avg = self.dists_avg / dist_sum
+
+    def load_matrix(self, pkl_path):
+        dists = np.load(pkl_path + 'dists.npy')
+        #dists_avg = np.load(pkl_path + 'dists_avg.npy')
+        counts = np.load(pkl_path + 'counts.npy')
+        #return dists, dists_avg, counts
+        return dists, counts
+
+    def dump_matrix(self, pkl_path):
+        np.save(pkl_path + 'dists.npy' , self.dists)
+        #np.save(pkl_path + 'dists_avg.npy' , self.dists_avg)
+        np.save(pkl_path + 'counts.npy' , self.counts)
 
 
 class Corpus(object):
@@ -102,36 +125,38 @@ class Vocabulary(object):
                            and i not in stop_words]
 
 
-def main():
+def main(corpus_dir):
     vocab = Vocabulary(VOCABULARY_FILE)
     cal = CalDist(len(vocab.words), WINDOW_SIZE)
 
-    corpus = Corpus(CORPUS_DIR)
+    corpus = Corpus(corpus_dir)
     
     j = 0
     for line_words in corpus:
-        if j == 100:
-            break
         j += 1
-        if j % 100 ==0:
+        if j % 1000 ==0:
             print j
-
         cal.traverse(line_words, vocab)
-
-    cal.cal_avg_dist()
-    a = cal.dists_avg
 
     b = cal.counts
     i,j = np.unravel_index(b.argmax(), b.shape)
     print 'i=%s, j=%s' % (i, j)
-    print vocab.get_word(i), vocab.get_word(j), cal.dists[i,j], cal.counts[i,j], a[i, j]
+    print vocab.get_word(i), vocab.get_word(j), cal.dists[i,j], cal.counts[i,j]
 
-    i,j = np.unravel_index(a.argmax(), a.shape)
-    print 'i=%s, j=%s' % (i, j)
-    print vocab.get_word(i), vocab.get_word(j), cal.dists[i,j], cal.counts[i,j], a[i, j]
+    time_str = datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
+    pkl_path = os.path.join(PKL_PATH, time_str+corpus_dir.split('/')[-2])
+    print 'pkl_path', pkl_path
+    cal.dump_matrix(pkl_path)
+
+    cal_load = CalDist(len(vocab.words), WINDOW_SIZE, pkl_path)
+    print np.array_equal(cal.dists, cal_load.dists)
+    print np.array_equal(cal.counts, cal_load.counts)
     
-    print cal.dists_avg.max()
-
 
 if __name__ == '__main__':
-    main()
+    CORPUS_DIR = [dir_prefix % i for i in tag]
+    CORPUS_DIR = CORPUS_DIR[16:]
+    print CORPUS_DIR
+    pool = Pool(processes=8)
+    pool.map(main, CORPUS_DIR)
+
